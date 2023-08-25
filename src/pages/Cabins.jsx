8cf0e39686da484d3dfdaa597fdfaae52142b4b7
Ronnie <ronnie.imagawa@gmail.com>
2023-08-25 21:01:37 +0200

    fetch cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    async function fetchCabins() {
+      const data = await getCabins();
+      console.log(data);
+    }
+    fetchCabins();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
